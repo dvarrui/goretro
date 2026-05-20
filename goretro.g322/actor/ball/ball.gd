8b0bc2d6034fc8a74ce6485d15992575f9983121
David Vargas <dvarrui@protonmail.com>
2026-05-20 13:44:02 +0100

    new goretro breakout

---
+++ goretro.g322/actor/ball/ball.gd
@@ -0,0 +1,24 @@
+extends RigidBody2D
+
+onready var sound_block = $sound/block
+onready var sound_wall = $sound/wall
+onready var sound_gameover = $sound/gameover
+
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	# Check collisions
+	for body in get_colliding_bodies():
+		if body.is_in_group("block"):
+			sound_block.pitch_scale = rand_range(1, 1.2)
+			sound_block.play()
+			body.hit()
+		else:
+			sound_wall.pitch_scale = rand_range(0.8, 1.2)
+			sound_wall.play()
+
+func _on_visibility_screen_exited():
+	sound_gameover.play()
+	yield(get_tree().create_timer(1.0), "timeout")
+	get_tree().change_scene("res://ui/title.tscn")
