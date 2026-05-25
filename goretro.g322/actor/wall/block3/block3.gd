323c0e8ba52e2b5fe1787d5ba11fcdb85703b0cb
David Vargas <dvarrui@protonmail.com>
2026-05-25 17:43:30 +0100

    blocks size3 red orange and yellow

---
+++ goretro.g322/actor/wall/block3/block3.gd
@@ -0,0 +1,43 @@
+extends StaticBody2D
+
+export var color = "red"
+var life = 1
+var hits = 0
+
+onready var sprite = $sprite/red
+onready var effect = $sprite/effect
+
+func _ready():
+	if color == "red":
+		sprite = $sprite/red
+		sprite.visible = true
+	elif color == "orange":
+		sprite = $sprite/orange
+		sprite.visible = true
+	elif color == "yellow":
+		sprite = $sprite/yellow
+		sprite.visible = true
+
+func play_effect():
+	var color_normal = Color(1, 1, 1, 1)
+	var color_brillo = Color(250, 250, 250, 1)
+	
+	effect.interpolate_property(sprite, "modulate",
+		color_normal, color_brillo, 0.1,
+		Tween.TRANS_SINE, Tween.EASE_OUT)
+	
+	# Animación de vuelta (0.2 segundos) con retraso
+	effect.interpolate_property(sprite, "modulate",
+		color_brillo, color_normal, 0.1,
+		Tween.TRANS_SINE, Tween.EASE_IN, 0.1)
+		
+	effect.start()
+
+func hit():
+	hits += 1 
+	play_effect()
+	if hits >= life: 
+		# Esperar un instante para que se vea el efecto antes de borrarlo
+		yield(effect, "tween_all_completed")
+		queue_free()
+
